b56ac8bb123ce57ec278bbc8b5c5d41d2a2b572e
Jean Bryant Figueroa <jbster559@gmail.com>
2025-01-24 15:06:15 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Author: Jean Bryant Figueroa
+//Course: COMP-003A
+//Facualty: Johnathan Cruz
+//Purpose: Demonstrate selection statements in C#.
+
+namespace COMP003A.LectureActivity3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
